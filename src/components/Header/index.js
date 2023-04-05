0b7ec8d86b37f3916b440fb6dc048c2ec66b743e
Felipe Morales Simões <100170616+FelipeMoralesdev@users.noreply.github.com>
2023-04-05 20:29:20 -0300

    start app coco e cia

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+import {Text, Image, View} from "react-native";
+import styles from "./style";
+
+
+
+export default function Header(){
+    return(
+        <View>
+            <Text style={styles.titulo}>Gerenciamento de Pedidos</Text>
+            
+        </View>
+        
+        
+    )
+    
+}
---
+++ src/components/Header/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    titulo: {
+        fontSize: 22,
+        flex: 1,
+        marginTop: 60,
+    }
+  
+
+});
+
+export default styles
