ece73f4783a942df06ce4be43edc397114e92db5
Felipe Morales Simões <100170616+FelipeMoralesdev@users.noreply.github.com>
2023-04-20 23:41:10 -0300

    add footer

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Header from './src/components/Header';
 import Body from './src/components/Body';
+import Footer from './src/components/Footer';
 import Produtos from './src/components/Body/Produtos';
 import Produtos2 from './src/components/Body/Produtos2';
 
@@ -10,15 +11,18 @@ export default function App() {
   return (
     <View style={styles.container}>
       <ScrollView style={styles.scroll}>
-      <Header/>
-      <Body/>
-      <Produtos/>
-      <Produtos2/>
-      <Produtos/>
+        <Header/>
+        <Body/>
+        <Produtos/>
+        <Produtos2/>
+        <Produtos/>
+        <Footer/>
       </ScrollView>
-      
+      <Footer/>
+
 
       
+
       <StatusBar style="auto" />
     </View>
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,62 @@
+import { StyleSheet, Text, Image, View, TouchableOpacity } from "react-native";
+
+export default function Footer() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>Faturamento total: R$</Text>
+      </View>
+      <View>
+        <Text>Pedido aprovado?</Text>
+      </View>
+      <View>
+        <Text>Peso total:</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Ver Carrinho</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button2}>
+          <Text style={styles.buttonText}>Fechar Pedido</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 80,
+    backgroundColor: "#fff",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 10,
+    marginBottom: 50,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "60%",
+    paddingTop: 5,
+  },
+  button: {
+    backgroundColor: "#FFCC4D",
+    height: 50,
+    width: "48%",
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button2: {
+    backgroundColor: "#005d22ff",
+    height: 50,
+    width: "48%",
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#FFF",
+    fontWeight: "bold",
+  },
+});
